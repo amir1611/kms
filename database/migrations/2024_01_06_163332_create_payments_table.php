feaca2d41ee5eb9754fecbe19ed5e959727e46e9
chiciko10 <imankai10@gmail.com>
2024-01-07 07:41:02 +0800

    aaaa

--- database/migrations/2024_01_06_163332_create_payments_table.php
+++ database/migrations/2024_01_06_163332_create_payments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->id('payment_id');
+            $table->id();
             $table->foreignId('kiosk_id')->constrained('kiosks');
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('application_id')->constrained('applications');
